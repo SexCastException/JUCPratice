19794c4cc145d78ff688a21e85654ceafb38ab01
pyh <1908421836@qq.com>
2022-03-28 12:10:39 +0800

    park test

--- src/main/java/com/huazai/juc/test/ParkTest.java
+++ src/main/java/com/huazai/juc/test/ParkTest.java
@@ -1,5 +1,6 @@
 package com.huazai.juc.test;
 
+import com.huazai.juc.test.utils.JucUtils;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.locks.LockSupport;
@@ -11,18 +12,20 @@ import java.util.concurrent.locks.LockSupport;
 public class ParkTest {
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(() -> {
-            log.debug("park...");
+            log.debug("第一次park开始，打断状态：{}", Thread.currentThread().isInterrupted());
             // park方法会一直堵塞，直到打断标志为true，打断 park 线程, 不会清空打断状态
             LockSupport.park();
             log.debug("unpark...");
-//            log.debug("打断状态：{}", Thread.currentThread().isInterrupted());
-            log.debug("打断状态：{}", Thread.interrupted());   // Thread.interrupted()会重置打断标志，导致第二次park时候堵塞
-            log.debug("第二次park开始...");
+            log.debug("打断状态：{}", Thread.currentThread().isInterrupted());
+//            JucUtils.sleepSecond(1);
+//            log.debug("打断状态：{}", Thread.interrupted());   // Thread.interrupted()会重置打断标志，导致第二次park时候堵塞
+            log.debug("第二次park开始，打断状态：{}", Thread.currentThread().isInterrupted());
             LockSupport.park();
             log.debug("第二次park结束...");
         }, "t1");
         t1.start();
         Thread.sleep(2000);
+        log.debug("主线程开始打断t1线程");
         t1.interrupt();
     }
 }
