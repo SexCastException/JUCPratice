9652d4669f636a79b75709cbacd9c33a97510979
pangyinghua <1908421836@qq.com>
2022-08-30 17:11:49 +0800

    Park 复习测试

--- src/main/java/com/huazai/juc/test/ParkTest.java
+++ src/main/java/com/huazai/juc/test/ParkTest.java
@@ -7,6 +7,9 @@ import java.util.concurrent.locks.LockSupport;
 
 /**
  * 打断 park 线程, 不会清空打断状态
+ * 重置打断状态方法：
+ * 1、Thread.interrupted()
+ * 2、
  */
 @Slf4j
 public class ParkTest {
@@ -20,10 +23,13 @@ public class ParkTest {
             LockSupport.park();
             log.debug("第二次park结束");
             log.debug("打断状态：{}", Thread.currentThread().isInterrupted());
-            log.debug("sleep，wait等阻塞api，不管是先阻塞后打断，还是先打断后阻塞，只要阻塞期间清空标志为true，都会抛出异常");
-            JucUtils.sleepSecond(1);
-            log.debug("Thread.interrupted()会重置打断标志，打断状态：{}", Thread.interrupted());   // Thread.interrupted()会重置打断标志，导致第二次park时候堵塞
+            resetInterruptedBySleep();
+//            resetInterrupted();
             log.debug("第三次park开始，打断状态：{}", Thread.currentThread().isInterrupted());
+            /*
+                如果是Thread.interrupted()重置打断标志，则park会一直阻塞，
+                如果是sleep，wait等方法被打断导致打断标志重置，则即使打断状态为false，执行park方法也不会阻塞。
+             */
             LockSupport.park();
             log.debug("第三次park结束...");
         }, "t1");
@@ -31,5 +37,18 @@ public class ParkTest {
         Thread.sleep(2000);
         log.debug("主线程开始打断t1线程");
         t1.interrupt();
+    }
+
+    /**
+     * 通过打断阻塞方法（sleep，wait等）重置打断标志
+     */
+    private static void resetInterruptedBySleep() {
+        log.debug("sleep，wait等阻塞api，不管是先阻塞后打断，还是先打断后阻塞，只要阻塞期间清空标志为true，都会抛出异常");
+        JucUtils.sleepSecond(1);
+        log.debug("阻塞被打断后，重置打断标志，打断状态：{}", Thread.currentThread().isInterrupted());
+    }
+
+    private static void resetInterrupted() {
+        log.debug("Thread.interrupted()会重置打断标志，打断状态：{}", Thread.interrupted());   // Thread.interrupted()会重置打断标志
     }
 }
