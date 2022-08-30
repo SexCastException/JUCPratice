5110a28d9034bf1c48414318750d9df2cedc6c20
pangyinghua <1908421836@qq.com>
2022-08-30 15:17:58 +0800

    FutureTaskTest & Interrupt 复习

--- src/main/java/com/huazai/juc/test/FutureTaskTest.java
+++ src/main/java/com/huazai/juc/test/FutureTaskTest.java
@@ -2,10 +2,12 @@ package com.huazai.juc.test;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
 
 /**
+ * 当一个线程需要等待另一个线程把某个任务执行完后它才能继续执行，此时可以使用FutureTask。
+ *
  * @author pyh
  * @datetime 2022/4/12 17:52
  * @description
@@ -14,11 +16,46 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class FutureTaskTest {
     public static void main(String[] args) throws Exception {
+//        futureTaskTest1();
+
+        futureTaskTest2();
+    }
+
+    private static void futureTaskTest2() throws ExecutionException, InterruptedException {
+        FutureTask<String> futureTaskA = new FutureTask<>(() -> {
+            System.out.println(Thread.currentThread().getName() + "在call方法里");
+            System.out.println(Thread.currentThread().getName() + "线程进入了 call方法，开始睡觉（进行了一些计算）");
+            Thread.sleep(10000);
+            System.out.println(Thread.currentThread().getName() + "睡醒了");
+            return Thread.currentThread().getName() + "返回的：" + System.currentTimeMillis();
+        });
+
+        FutureTask<String> futureTaskB = new FutureTask<>(() -> {
+            String s = futureTaskA.get();
+            System.out.println("futureTaskB result" + s);
+            System.out.println(Thread.currentThread().getName() + "在call方法里");
+            return Thread.currentThread().getName() + "返回的：" + System.currentTimeMillis();
+        });
+
+        new Thread(futureTaskA, "线程A").start();
+        new Thread(futureTaskB, "线程B").start();
+
+        while (!futureTaskB.isDone()) {  //isDone表示FutureTask的计算是否完成
+        }
+        System.out.println(futureTaskA.get());
+        System.out.println(futureTaskB.get());
+
+        System.out.println(Thread.currentThread().getName() + "结束了");
+    }
+
+
+    private static void futureTaskTest1() throws InterruptedException, ExecutionException {
         FutureTask<String> futureTask = new FutureTask(() -> {
+            log.debug("子线程执行");
             return "hello world";
         });
 
-        new Thread().start();
+        new Thread(futureTask).start();
 
         String result = futureTask.get();
 
